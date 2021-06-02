fed1c8a40f9a0be11b0b6e43980bf0a4cb06527f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-02 10:29:43 -0700

    completed code for getting last workout from the database

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
+const Workouts = require('../models/workouts');
+
+router.get('/api/workouts', (req, res) => {
+    Workouts.find({})
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 
 module.exports = router;
