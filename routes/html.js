b5f1de9036c2682adc0ff1a58c6d7c027dd5ef77
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-07 11:08:49 -0700

    comments added to html.js file in routes folder

--- routes/html.js
+++ routes/html.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const path = require('path');
 
+//html get request to render the stats page for clients to access
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'))
 });
 
+//html get request to render the exercise page for clients to access
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'))
 });
 
+//exports the routes for other modules to call upon
 module.exports = router;
